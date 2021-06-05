85eeca852a4de753aa3488a0e07e8cc78a92a259
bonapartbon <bonapathb1@gmail.com>
2021-06-06 00:10:00 +0700

    db upload

---
+++ database/cinema_db.sql
@@ -0,0 +1,155 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 05, 2021 at 07:08 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cinema_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookingtable`
+--
+
+CREATE TABLE `bookingtable` (
+  `bookingID` int(11) NOT NULL,
+  `movieName` varchar(100) DEFAULT NULL,
+  `bookingTheatre` varchar(100) NOT NULL,
+  `bookingType` varchar(100) DEFAULT NULL,
+  `bookingDate` varchar(50) NOT NULL,
+  `bookingTime` varchar(50) NOT NULL,
+  `bookingFName` varchar(100) NOT NULL,
+  `bookingLName` varchar(100) DEFAULT NULL,
+  `bookingPNumber` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bookingtable`
+--
+
+INSERT INTO `bookingtable` (`bookingID`, `movieName`, `bookingTheatre`, `bookingType`, `bookingDate`, `bookingTime`, `bookingFName`, `bookingLName`, `bookingPNumber`) VALUES
+(25, 'Captain Marvel', 'main-hall', '3d', '14-3', '09-00', 'Bon', 'Bonapart', '0967978856');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedbacktable`
+--
+
+CREATE TABLE `feedbacktable` (
+  `msgID` int(12) NOT NULL,
+  `senderfName` varchar(50) NOT NULL,
+  `senderlName` varchar(50) DEFAULT NULL,
+  `sendereMail` varchar(100) NOT NULL,
+  `senderfeedback` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `feedbacktable`
+--
+
+INSERT INTO `feedbacktable` (`msgID`, `senderfName`, `senderlName`, `sendereMail`, `senderfeedback`) VALUES
+(1, 'Ahmed', 'Ali', 'Ahmed@mail.com', 'Hello first'),
+(2, 'Ahmed', 'Ali', 'asa@as.com', 'asdas');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movietable`
+--
+
+CREATE TABLE `movietable` (
+  `movieID` int(11) NOT NULL,
+  `movieImg` varchar(150) NOT NULL,
+  `movieTitle` varchar(100) NOT NULL,
+  `movieGenre` varchar(50) NOT NULL,
+  `movieDuration` int(11) NOT NULL,
+  `movieRelDate` date NOT NULL,
+  `movieDirector` varchar(50) NOT NULL,
+  `movieActors` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `movietable`
+--
+
+INSERT INTO `movietable` (`movieID`, `movieImg`, `movieTitle`, `movieGenre`, `movieDuration`, `movieRelDate`, `movieDirector`, `movieActors`) VALUES
+(1, 'img/movie-poster-1.jpg', 'Captain Marvel', ' Action, Adventure, Sci-Fi ', 220, '2018-10-18', 'Anna Boden, Ryan Fleck', 'Brie Larson, Samuel L. Jackson, Ben Mendelsohn'),
+(2, 'img/movie-poster-2.jpg', 'Qarmat Bitamrmat  ', 'Comedy', 110, '2018-10-18', 'Assad Fouladkar', 'Ahmed Adam, Bayyumy Fouad, Salah Abdullah , Entsar, Dina Fouad '),
+(3, 'img/movie-poster-3.jpg', 'The Lego Movie', 'Animation, Action, Adventure', 110, '2014-02-07', 'Phil Lord, Christopher Miller', 'Chris Pratt, Will Ferrell, Elizabeth Banks'),
+(4, 'img/movie-poster-4.jpg', 'Nadi Elregal Elserri ', 'Comedy', 105, '2019-01-23', ' Ayman Uttar', 'Karim Abdul Aziz, Ghada Adel, Maged El Kedwany, Nesreen Tafesh, Bayyumy Fouad, Moataz El Tony '),
+(5, 'img/movie-poster-5.jpg', 'VICE', 'Biography, Comedy, Drama', 132, '2018-12-25', 'Adam McKay', 'Christian Bale, Amy Adams, Steve Carell'),
+(6, 'img/movie-poster-6.jpg', 'The Vanishing', 'Crime, Mystery, Thriller', 107, '2019-01-04', 'Kristoffer Nyholm', 'Gerard Butler, Peter Mullan, Connor Swindells'),
+(12, 'img/', 'Bunkheng The Movie', 'Funny', 120, '2021-06-10', 'Try Many', 'Vyrayuth'),
+(13, 'img/', 'Bunkheng The Movie', 'Funny', 123, '2021-06-24', 'Try Many', 'Vyrayuth');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bookingtable`
+--
+ALTER TABLE `bookingtable`
+  ADD PRIMARY KEY (`bookingID`),
+  ADD UNIQUE KEY `bookingID` (`bookingID`),
+  ADD KEY `bookingID_2` (`bookingID`),
+  ADD KEY `bookingID_3` (`bookingID`),
+  ADD KEY `bookingID_4` (`bookingID`);
+
+--
+-- Indexes for table `feedbacktable`
+--
+ALTER TABLE `feedbacktable`
+  ADD PRIMARY KEY (`msgID`),
+  ADD UNIQUE KEY `msgID` (`msgID`);
+
+--
+-- Indexes for table `movietable`
+--
+ALTER TABLE `movietable`
+  ADD PRIMARY KEY (`movieID`),
+  ADD UNIQUE KEY `movieID` (`movieID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bookingtable`
+--
+ALTER TABLE `bookingtable`
+  MODIFY `bookingID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `feedbacktable`
+--
+ALTER TABLE `feedbacktable`
+  MODIFY `msgID` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `movietable`
+--
+ALTER TABLE `movietable`
+  MODIFY `movieID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
