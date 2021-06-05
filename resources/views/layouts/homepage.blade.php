2a3c1c6314ac54f1f18fea6fdacca87ceac78580
bonapartbon <bonapathb1@gmail.com>
2021-06-06 00:06:12 +0700

    homepage intergration

---
+++ resources/views/layouts/homepage.blade.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/styles.css">
+    <link rel="stylesheet" href="style/style.css">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <title>ParaGone Cineplex | Home</title>
+    <link rel="icon" type="image/png" href="img/logo.png">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+    
+    <?php
+    $link = mysqli_connect("localhost", "root", "", "cinema_db");
+    $sql = "SELECT * FROM movieTable";
+    ?>
+    
+        @include('layouts.includes.header')
+
+        <div id="home-section-1" class="movie-show-container">
+        
+        <h1>Currently Showing</h1>
+        <h3>Book a movie now</h3>
+
+        <div class="movies-container">
+
+            <?php
+                        if($result = mysqli_query($link, $sql)){
+                            if(mysqli_num_rows($result) > 0){
+                                for ($i = 0; $i <= 5; $i++){
+                                    $row = mysqli_fetch_array($result);
+                                    echo '<div class="movie-box">';
+                                    echo '<img src="'. $row['movieImg'] .'" alt=" ">';
+                                    echo '<div class="movie-info ">';
+                                    echo '<h3>'. $row['movieTitle'] .'</h3>';
+                                    echo '<a href="booking.php?id='.$row['movieID'].'"><i class="fas fa-ticket-alt"></i> Book a seat</a>';
+                                    echo '</div>';
+                                    echo '</div>';
+                                }
+                                mysqli_free_result($result);
+                            } else{
+                                echo '<h4 class="no-annot">No Booking to our movies right now</h4>';
+                            }
+                        } else{
+                            echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+                        }
+                        
+                        // Close connection
+                        mysqli_close($link);
+                        ?>
+        </div>
+    </div>
+
+    <div id="home-section-2" class="services-section">
+        <h1>How it works</h1>
+        <h3>3 Simple steps to book your favourit movie!</h3>
+
+        <div class="services-container">
+            <div class="service-item">
+                <div class="service-item-icon">
+                    <i class="fas fa-4x fa-video"></i>
+                </div>
+                <h2>1. Choose your favourite movie</h2>
+                <p>choose the movie you want to watch at our theaters</p>
+            </div>
+            <div class="service-item">
+                <div class="service-item-icon">
+                    <i class="fas fa-4x fa-credit-card"></i>
+                </div>
+                <h2>2. Pay for your tickets</h2>
+                <p>choose the movie you want to watch at our theaters</p>
+            </div>
+            <div class="service-item">
+                <div class="service-item-icon">
+                    <i class="fas fa-4x fa-theater-masks"></i>
+                </div>
+                <h2>3. Pick your seats & Enjoy watching</h2>
+                <p>choose the movie you want to watch at our theaters</p>
+            </div>
+            <div class="service-item"></div>
+            <div class="service-item"></div>
+        </div>
+    </div>
+
+
+
+
+    
+    <footer class="copyright"> 
+    © COPYRIGHT RESERVED © 2021 PARAGONE CINEPLEX. ALL RIGHTS RESERVED. 
+    </footer>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+    
+    <script src="scripts/jquery-3.3.1.min.js "></script>
+    <script src="scripts/script.js "></script>
+</body>
+
+</html>
---
+++ resources/views/layouts/includes/header.php
@@ -0,0 +1,36 @@
+  <!-- ======= Header/Navbar ======= -->
+  <nav class="navbar navbar-default navbar-trans navbar-expand-lg fixed-top">
+    <div class="container">
+      <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarDefault" aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <a class="navbar-brand text-brand" href="index.html"><img src="img/paragone-logo.png" width="15%"> </a>
+
+      <div class="navbar-collapse collapse justify-content-center" id="navbarDefault">
+        <ul class="navbar-nav">
+
+          <li class="nav-item">
+            <a class="nav-link active" href="index.html">Home</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link " href="about.html">Showtimes</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link " href="contact.html">Contact</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link " href="contact.html">Login</a>
+          </li>
+        </ul>
+      </div>
+
+     
+
+    </div>
+  </nav>
+  <!-- End Header/Navbar -->
