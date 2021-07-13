4230224b5412b865108f20d807899e20373c7b47
bunkheng <kenmasters765@gmail.com>
2021-07-13 17:23:31 +0700

    one last last last change

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -21,15 +21,8 @@ class UsersSeeder extends Seeder
                 'is_admin' => '1',
                 'name' => 'Admin',
                 'email_verified_at' => '2021-06-21 03:59:08',
-                'email' => 'admin@paragone.com',
+                'email' => 'paragonecineplex@gmail.com',
                 'password' => '$2y$10$5nlpheg6mh6mcTTjyaJLtebcgu.u02b23mid1TJ1T1ad6xaIJyU02'
-            ],
-            [
-                'is_admin' => '0',
-                'name' => 'User',
-                'email_verified_at' => '2021-06-21 04:00:25',
-                'email' => 'user@paragone.com',
-                'password' => '$2y$10$OmJol6eTtho/KpWC86m8wuFT67NmCx.4Zy1cAmaBeyfT7GfS/8xfq'
             ]
         ];
 
