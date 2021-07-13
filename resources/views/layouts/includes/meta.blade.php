fc726451a0ec1627dfae8f11c2c0df077d0fcb10
bunkheng <kenmasters765@gmail.com>
2021-07-13 15:08:07 +0700

    changes happen

--- resources/views/layouts/includes/meta.blade.php
+++ resources/views/layouts/includes/meta.blade.php
@@ -1,9 +1,9 @@
 <!-- Required meta tags -->
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link rel="icon" type="image/png" href="{{asset('img/paragone-favicon.png')}}">
+<link rel="icon" type="image/png" href="{{secure_('img/paragone-favicon.png')}}">
 <!-- Template CSS -->
-<link rel="stylesheet" href="{{asset('template-css/style-starter.css')}}">
+<link rel="stylesheet" href="{{secure_asset('template-css/style-starter.css')}}">
 <!-- Template CSS -->
 <link href="//fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,600&display=swap" rel="stylesheet">
 <!-- Template CSS -->
