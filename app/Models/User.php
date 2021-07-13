0bff760ef13d20abf761a2c4e356da30fa674054
bunkheng <kenmasters765@gmail.com>
2021-07-13 16:41:48 +0700

    add last change

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable implements 
 {
-    use HasFactory, Notifiable;
+    use HasFactory;
 
     /**
      * The attributes that are mass assignable.
