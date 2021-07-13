950fbfe6b5b3f680460c91e9169c08d35b00fdfe
bunkheng <kenmasters765@gmail.com>
2021-07-13 16:37:51 +0700

    add last change

--- routes/web.php
+++ routes/web.php
@@ -41,5 +41,6 @@ Route::get('admin/upcomings', 'App\Http\Controllers\MovieController@upcoming')->
 Route::resource('admin/contact', 'App\Http\Controllers\ContactController')->middleware('is_admin');
 
 // Verify Register Route
+Auth::routes(['verify' => true]);
 Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
